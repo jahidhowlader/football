a9fc7f66a9e0039cf350036b0cc5ad09b5770391
jahidhowlader <jahidhowlader.info@gmail.com>
2023-02-24 12:24:20 +0600

    try create navbar

--- script/app.js
+++ script/app.js
@@ -1,3 +1,16 @@
-document.getElementById("team-a").addEventListener('click', function(){
-    window.location.href = 'global-ranking.html'
+document.getElementById("hamm").addEventListener('click', function(){
+   const navbar = document.getElementById('navbar')
+   navbar.style.display = 'block'
+
+   const hamburger  = document.getElementById('hamburger')
+   hamburger.style.display = 'none'
+
+})
+
+document.getElementById('closed').addEventListener('click', function(){
+    const navbar = document.getElementById('navbar')
+   navbar.style.display = 'none'
+
+   const hamburger  = document.getElementById('hamburger')
+   hamburger.style.display = 'block'
 })
