5b893932853827419764cf609d6fcbc306ee51cf
jahidhowlader <jahidhowlader.info@gmail.com>
2023-03-07 22:53:50 +0600

    update design part

--- script/app.js
+++ script/app.js
@@ -1,24 +1,3 @@
-// document.getElementById("hamm").addEventListener('click', function(){
-//    const navbar = document.getElementById('navbar')
-//    navbar.style.display = 'block'
-
-//    const hamburger  = document.getElementById('hamburger')
-//    hamburger.style.display = 'none'
-
-// })
-
-// document.getElementById('closed').addEventListener('click', function(){
-//     const navbar = document.getElementById('navbar')
-//    navbar.style.display = 'none'
-
-//    const hamburger  = document.getElementById('hamburger')
-//    hamburger.style.display = 'block'
-// })
-
-document.getElementById('menu').addEventListener('click', function(){
-
-})
-
 /* Open when someone clicks on the span element */
 function openNav() {
    document.getElementById("myNav").style.width = "100%";
