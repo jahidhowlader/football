5d63ae0cee9cfb715e14cb1109fcac775a5a5684
jahidhowlader <jahidhowlader.info@gmail.com>
2023-03-07 22:20:51 +0600

    start new nav

--- script/app.js
+++ script/app.js
@@ -1,16 +1,17 @@
-document.getElementById("hamm").addEventListener('click', function(){
-   const navbar = document.getElementById('navbar')
-   navbar.style.display = 'block'
+// document.getElementById("hamm").addEventListener('click', function(){
+//    const navbar = document.getElementById('navbar')
+//    navbar.style.display = 'block'
 
-   const hamburger  = document.getElementById('hamburger')
-   hamburger.style.display = 'none'
+//    const hamburger  = document.getElementById('hamburger')
+//    hamburger.style.display = 'none'
 
-})
+// })
 
-document.getElementById('closed').addEventListener('click', function(){
-    const navbar = document.getElementById('navbar')
-   navbar.style.display = 'none'
+// document.getElementById('closed').addEventListener('click', function(){
+//     const navbar = document.getElementById('navbar')
+//    navbar.style.display = 'none'
 
-   const hamburger  = document.getElementById('hamburger')
-   hamburger.style.display = 'block'
-})
+//    const hamburger  = document.getElementById('hamburger')
+//    hamburger.style.display = 'block'
+// })
+
